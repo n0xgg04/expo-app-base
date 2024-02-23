95097f7818293b45c4887486dac2e573042c8502
bobbibao <lehoangbao1125@gmail.com>
2024-02-24 04:13:26 +0700

    Features: Adding a new screen named navigation_alert_screen with a demo image in assets/image/demo

--- app/global/constants/screens.ts
+++ app/global/constants/screens.ts
@@ -3,5 +3,6 @@ export const SCREEN_NAME = {
   home_feed: "homezone.feed",
   info: "info",
   get_started: "get_started",
+  NavigationAlertScreen: "navigation_alert_screen",
   //TODO : Add here
 };
